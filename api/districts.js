37ed1d36eedef5742a81367a4aa6b197996b9d2a
BossBele <adambeleko@gmail.com>
2021-11-26 20:08:53 +0300

    fuzzysort

--- api/districts.js
+++ api/districts.js
@@ -1,11 +1,25 @@
 const { Router } = require('express')
+const fuzzysort = require('fuzzysort')
 
 // resource
 const resource = require('../content/districts.json')
 
 const router = Router()
 
-router.use('/districts', (req, res) => {
+router.get('/', (req, res) => {
+  const { search } = req.query
+  if (!search) {
+    return res.json(resource)
+  }
+
+  const targets = fuzzysort.go(search, resource, {
+    keys: ['districtName']
+  })
+
+  const results = targets.map(({ obj }) => obj)
+  return res.json(results)
+})
+router.post('/', (req, res) => {
   res.json(resource)
 })
 

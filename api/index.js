df1b441de66c50e13c96a0340dcd74dd61f951e3
BossBele <adambeleko@gmail.com>
2021-11-26 20:08:56 +0300

    fuzzysort

--- api/index.js
+++ api/index.js
@@ -2,16 +2,16 @@ const express = require('express')
 const app = express()
 
 const countries = require('./countries')
-app.use(countries)
+app.use('/countries', countries)
 
 const regions = require('./regions')
-app.use(regions)
+app.use('/regions', regions)
 
 const districts = require('./districts')
-app.use(districts)
+app.use('/districts', districts)
 
 const wards = require('./wards')
-app.use(wards)
+app.use('/wards', wards)
 
 if (require.main === module) {
   const port = 3001

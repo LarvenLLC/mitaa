b35361fd5793ed38f5dfe2b491c9bd8302fc6d40
BossBele <adambeleko@gmail.com>
2021-11-26 20:08:50 +0300

    fuzzysort

--- api/countries.js
+++ api/countries.js
@@ -1,11 +1,25 @@
 const { Router } = require('express')
+const fuzzysort = require('fuzzysort')
 
 // resource
 const resource = require('../content/countries.json')
 
 const router = Router()
 
-router.use('/countries', (req, res) => {
+router.get('/', (req, res) => {
+  const { search } = req.query
+  if (!search) {
+    return res.json(resource)
+  }
+
+  const targets = fuzzysort.go(search, resource, {
+    keys: ['iso', 'name', 'nicename', 'iso3', 'phonecode']
+  })
+
+  const results = targets.map(({ obj }) => obj)
+  return res.json(results)
+})
+router.post('/', (req, res) => {
   res.json(resource)
 })
 

fd69f0c298af200af64f9f09b72069e6d8f8b74a
BossBele <adambeleko@gmail.com>
2021-11-26 15:49:56 +0300

    api

--- nuxt.config.js
+++ nuxt.config.js
@@ -52,5 +52,12 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+  },
+
+  serverMiddleware: {
+    '/api': '~/api'
   }
+
+  //
+  // middleware: ['countries']
 }

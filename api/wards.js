9da2759df9ff0b613be880ca195fb147ba3394bd
BossBele <adambeleko@gmail.com>
2021-11-26 20:09:01 +0300

    fuzzysort

--- api/wards.js
+++ api/wards.js
@@ -1,11 +1,26 @@
 const { Router } = require('express')
+const fuzzysort = require('fuzzysort')
 
 // resource
 const resource = require('../content/wards.json')
+const districts = require('../content/districts.json')
 
 const router = Router()
 
-router.use('/wards', (req, res) => {
+router.get('/', (req, res) => {
+  const { search } = req.query
+  if (!search) {
+    return res.json(resource)
+  }
+
+  const targets = fuzzysort.go(search, resource, {
+    keys: ['wardName']
+  })
+
+  const results = targets.map(({ obj }) => ({ ...obj, district: districts.find(({ id }) => id === obj.district) }))
+  return res.json(results)
+})
+router.post('/', (req, res) => {
   res.json(resource)
 })
 
